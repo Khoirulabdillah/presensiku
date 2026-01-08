7b848c73153e6e7c4551c1306f7ba79cd288f275
khoirul <l200220029@student.ums.ac.id>
2026-01-08 15:39:49 +0700

    perbikan faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'username' => $this->faker->userName(),
+            'name' => ($this->faker ?? \Faker\Factory::create())->name(),
+            'username' => ($this->faker ?? \Faker\Factory::create())->userName(),
             'password' => Hash::make('password'),
             'role' => 'user',
             'remember_token' => Str::random(10),
